f78c3a1efca476d764275404594a05d393d546d7
MF-Ocsb <97174283+MF-Ocsb@users.noreply.github.com>
2022-01-14 14:40:59 +0000

    finished my assignment

---
+++ UNIT 4/U4A2/carpet.py
@@ -0,0 +1,21 @@
+# NAME OF AUTHOR: Matthew F
+# NAME OF THE PROGRAM: Carpet Cost Calculator
+# DATE OF CREATION:  January 13 2022
+# PURPOSE OF PROGRAM: Calculating carpet cost 
+
+# VARIABLE DEFINITION
+
+# INPUT
+print("Hello User! Please enter the length and width measurements to   calculate the cost.")
+print("Please enter the length (Meters)")
+length = int(input())
+print("Please enter the width (Meters)")
+width = int(input())
+
+# PROCESSING
+area = length * width
+cost = area * 2.25
+
+# OUTPUT
+print("The cost of your carpet is:")
+print(cost)
